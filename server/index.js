c2ccbdbb00b27a8ce426c924cd7264dcb7f7b14d
Msole18 <miguel.sole18@gmail.com>
2025-01-15 15:44:43 -0300

    feat: implement image generation and post creation functionality; add DALL-E and post routes, and enhance MongoDB connection error handling

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import express from 'express'
 import { connectDB } from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config() //allow to put env variables in .env file
 
 const app = express() //initialize express
 
-app.use(cors()) //allow cross origin requests
+app.use(cors({ origin: '*' })) //allow cross origin requests
 app.use(express.json({ limit: '50mb' })) //
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
 
 app.get('/', (req, res) => {
   res.send('Last day of the year')
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -4,7 +4,11 @@ export const connectDB = (url) => {
   mongoose.set('strictQuery', true) 
   
   // Connect to MongoDB
-  mongoose.connect(url)
-    .then(() => console.log('MoggoDB connected'))
-    .catch((err) => console.log(err))
+  mongoose
+    .connect(url)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => {
+      console.error('failed to connect with mongo')
+      console.error(err)
+    })
 }
